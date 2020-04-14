b2c408b2d480c43874797ef69727bc2429e27a3d
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-04-14 12:57:51 +0400

    init typeit

---
+++ assets/js/typed.js
@@ -0,0 +1,15 @@
+$('#typeanim').typeIt({
+        speed: 125,
+        breakLines: false,
+        autoStart: false,
+        loop: true,
+        html: true,
+    })
+    .tiType('Продвигаем.')
+    .tiPause(3000)
+    .tiDelete(11)
+    .tiType('Развиваем.')
+    .tiPause(3000)
+    .tiDelete(10)
+    .tiType('Создаем.')
+    .tiPause(3000)
